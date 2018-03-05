5a3a3638840906132a313564116adf816ce218e1
张海彪 <1473209216@qq.com>
2018-03-05 15:54:48 +0800

    Create Solution59.java

---
+++ src/main/java/com/biubiu/Solution59.java
@@ -0,0 +1,58 @@
+package com.biubiu.offer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 按之字形顺序打印二叉树
+ * Created by zhanghaibiao on 2018/3/5.
+ */
+public class Solution59 {
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (pRoot == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        int flag = -1;
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            while (queue.size() > 0) {
+                TreeNode node = queue.poll();
+                arrayList.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            if (flag == 1) Collections.reverse(arrayList);
+            flag = -flag;
+            result.add(arrayList);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(0);
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+        TreeNode treeNode3 = new TreeNode(3);
+        treeNode1.left = treeNode3;
+        TreeNode treeNode4 = new TreeNode(4);
+        treeNode2.left = treeNode4;
+        new Solution59().Print(treeNode);
+    }
+
+}
